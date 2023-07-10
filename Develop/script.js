42c7bdac2459ee7555a3154d18f432d573faf8f4
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-07-09 20:07:06 -0500

    Updated JavaScript and made small changes to HTML format

--- Develop/script.js
+++ Develop/script.js
@@ -72,49 +72,183 @@ function generateQuestion() {
 currentQuestionIndex = 0;
 generateQuestion();
 
-currentQuestionIndex++
-generateQuestion();
+//currentQuestionIndex++
+//generateQuestion();
 
 // Check answers.
 function correctAnswer() {
-    document.getElementById(currentQuestionIndex).style.backgroundColor = 'green';
+    // generate correct message with line above here
 }
 
 function wrongAnswer() {
-    document.getElementById(currentQuestionIndex).style.backgroundColor = 'green';
+    // generate wrong message with line above it here
 }
 
 // Timer
 let timeLeft = 75;
-function runTimer() {
-    timer.textContent = timeLeft;
+/*function runTimer() {
+    timer.textContent = 'Time: ' + timeLeft;
     if (timeLeft <= 0) {
         timeUp();
+    }
+}*/
+
+/*function timeCut() {
+    timeLeft--;
+    if (timeLeft <= 0) {
+        timeUp();
+    }
+}*/
+let timerInterval;
+
+function startTime() {
+    runTimer();
+    timerInterval = setInterval(function() {
+        runTimer();
+        timeCut();
+    }, 1000);
+
+    function runTimer() {
+        timer.textContent = 'Time: ' + timeLeft;
+        if (timeLeft <= 0) {
+            timeUp();
+        }
+    }
+
+    function timeCut() {
+        timeLeft--;
+        if (timeLeft <= 0) {
+            timeUp();
+        }
     }
 }
 
 function timeUp() {
-    console.log('Time is up!')
+    clearInterval(timerInterval);
     quiz.style.display = 'none';
     allDone.style.display = 'block';
-    clearInterval(timerInterval);
+    renderScore();
 }
 
-function timeCut() {
-    timeLeft--;
-    if (wrongAnswer()) {
-        timeLeft -= 10; }
-    if (timeLeft <= 0) {
+let score = 0;
+function checkAnswer(answer) {
+    const selectedAnswer = answer;
+    if (questions[currentQuestionIndex].correct === selectedAnswer) {
+        score += 20;
+        correctAnswer();
+        updateScore();
+    } else if (questions[currentQuestionIndex].correct !== selectedAnswer) {
+        timeLeft -= 10; 
+        wrongAnswer();
+    }
+    if (currentQuestionIndex < questionIndex) {
+        currentQuestionIndex++
+        generateQuestion();
+    } else {
         timeUp();
+        renderScore();
+    }
+}
+const startTitle = document.createElement('h3');
+startTitle.textContent = 'Coding Quiz Challenge';
+start.appendChild(startTitle);
+
+const startP = document.createElement('p');
+startP.textContent = 'Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
+startTitle.appendChild(startP);
+
+const startBttn = document.createElement('button');
+startBttn.textContent = 'Start Quiz';
+startBttn.id = 'startBttn';
+start.appendChild(startBttn);
+startBttn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    start.style.display = "none";
+    quiz.style.display = "block";
+    startTime();
+    generateQuestion();
+}
+
+function renderScore() {
+    if (currentQuestionIndex >= questionIndex || timeLeft <= 0) {
+        quiz.style.display = "none";
+        allDone.style.display = "block";
     }
 }
 
-function startTime() {
-    runTimer();
-    var timerInterval = setInterval(function() {
-        timeCut();
-        runTimer();
-    }, 1000);
-}
-startTime();
+const allDoneHeader = document.createElement('h3');
+    allDoneHeader.textContent = 'All Done!';
+    allDone.appendChild(allDoneHeader);
 
+function updateScore() {
+    scoreUpdate.textContent = 'Your final score is ' + score + '.';
+}
+    const scoreUpdate = document.createElement('p');
+    scoreUpdate.textContent = 'Your final score is ' + score + '.';
+    allDone.appendChild(scoreUpdate);
+    
+    const initialsEntry = document.createElement('p');
+    initialsEntry.textContent = 'Enter initials:';
+    allDone.appendChild(initialsEntry);
+
+    const inputBox = document.createElement('input');
+    inputBox.setAttribute('type', 'text');
+    inputBox.id = 'inputBox';
+    allDone.appendChild(inputBox);
+
+    const submitBttn = document.createElement('button');
+    submitBttn.textContent = 'Submit';
+    submitBttn.id = 'submitBttn';
+    allDone.appendChild(submitBttn);
+    submitBttn.addEventListener('click', handleSubmit);
+
+    let jsonData;
+    let data;
+    function handleSubmit() {
+        const initials = inputBox.value;
+        data = { initials, score };
+        jsonData = JSON.stringify(data);
+        localStorage.setItem('highScores', jsonData);
+        allDone.style.display = 'none';
+        scorePage.style.display = "block";
+    
+        renderScoreList();
+    }
+    
+    const scoreTitle = document.createElement('h3');
+    scoreTitle.textContent = 'High Scores';
+    scorePage.appendChild(scoreTitle);
+    
+    function renderScoreList() {
+        const scoreList = document.createElement('ol');
+        scorePage.appendChild(scoreList);
+        scoreList.innerHTML = '';
+        localStorage.getItem('highScores');
+        JSON.parse(jsonData);
+        const listItem = document.createElement('li');
+        listItem.textContent = data.initials + ' - ' + data.score;
+        scoreList.appendChild(listItem);
+
+        const goBackBttn = document.createElement('button');
+    goBackBttn.textContent = 'Go Back';
+    goBackBttn.id = 'goBackBttn';
+    scorePage.appendChild(goBackBttn);
+
+    const clearHighScores = document.createElement('button');
+    clearHighScores.textContent = 'Clear High Scores';
+    clearHighScores.id = 'clearHighScores';
+    scorePage.appendChild(clearHighScores);
+    }
+
+// Break Down
+// Select all HTML elements
+// Create questions array
+// Generate questions
+// Check answers logic
+// Logic to handle timer and time cuts
+// Determine if I want to display the right/wrong answer confirmation like the demo
+// Create div for answer check display ::after with line and results of check answer function
+// generate and append questions to the doc with one question appearing after the next one is answered. 
+// Generate and append "All Done" page and create submission for high score that saves to local storage. 
+// Generate and append high scores page that is pulling from local storage to create an ordered list. Use prevent default here. This will have a "go back" button that restarts the quiz and a clear high scores button that clears local storage. 
